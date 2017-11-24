8237baf05a78b62bc82739ed392abaf02e85477d
Sebastien CLOLUS <sclolus@e3r6p19.42.fr>
2017-11-24 10:52:10 +0100

    Ajout de -q: quiet mode

--- srcs/print_script_end.c
+++ srcs/print_script_end.c
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 09:34:50 by sclolus           #+#    #+#             */
-/*   Updated: 2017/11/24 09:48:23 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/11/24 10:51:34 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,13 @@ inline void	print_script_end(int typescript_fd, t_script_info *info)
 		ft_error(1, (char*[]){"Failed to gettimeofday()"}, 0);
 		_exit(EXIT_FAILURE);
 	}
-	ft_putchar_fd('\n', typescript_fd);
-	ft_putstr_fd(SCRIPT_END_FILE, typescript_fd);
-	ft_putstr_fd(time_str, typescript_fd);
-	ft_putstr(SCRIPT_END_STDOUT);
-	ft_putstr(info->file_script);
-	ft_putchar('\n');
+	if (!info->flags.bits.quiet)
+	{
+		ft_putchar_fd('\n', typescript_fd);
+		ft_putstr_fd(SCRIPT_END_FILE, typescript_fd);
+		ft_putstr_fd(time_str, typescript_fd);
+		ft_putstr(SCRIPT_END_STDOUT);
+		ft_putstr(info->file_script);
+		ft_putchar('\n');
+	}
 }
--- srcs/print_script_start.c
+++ srcs/print_script_start.c
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 09:13:14 by sclolus           #+#    #+#             */
-/*   Updated: 2017/11/24 09:47:16 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/11/24 10:51:18 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,12 @@ inline void	print_script_start(int typescript_fd, t_script_info *info)
 		ft_error(1, (char*[]){"Failed to gettimeofday()"}, 0);
 		_exit(EXIT_FAILURE);
 	}
-	ft_putstr_fd(SCRIPT_START_FILE, typescript_fd);
-	ft_putstr_fd(time_str, typescript_fd);
-	ft_putstr(SCRIPT_START_STDOUT);
-	ft_putstr(info->file_script);
-	ft_putchar('\n');
+	if (!info->flags.bits.quiet)
+	{
+		ft_putstr_fd(SCRIPT_START_FILE, typescript_fd);
+		ft_putstr_fd(time_str, typescript_fd);
+		ft_putstr(SCRIPT_START_STDOUT);
+		ft_putstr(info->file_script);
+		ft_putchar('\n');
+	}
 }
